664191552f5225a7a0ddae16eb2b821aeffd0813
AbhishekUnthread <abhishek.k@unthread.world>
2024-01-15 18:11:58 +0530

    prod env added

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    BASE_URL: 'https://shoe-craze-server.onrender.com/api/v1',
+    EMAIL: 'test-admin@gmail.com',
+    PASSWORD: 'Test@123',
+    ACCESS_EXPIRATION_TIME: '3600000',
+    TIME_BUFFER: '300000',
+    ENV: 'production',
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
